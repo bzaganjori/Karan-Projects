a73ca2c5c2138454bb14adf2b212a9c843795083
BZaganjori <bzaganjori@users.noreply.github.com>
2015-05-19 00:24:35 -0400

    Update changeReturnProgram.java

--- numbers/changeReturnProgram.java
+++ numbers/changeReturnProgram.java
@@ -2,6 +2,7 @@ package numbers;
 
 import java.util.Scanner;
 
+/** User enters the price and amount paid for an item in the console. Returns change due. */
 public class changeReturnProgram {
 	
 	private static String calculateChange(double cost, double paid) {
