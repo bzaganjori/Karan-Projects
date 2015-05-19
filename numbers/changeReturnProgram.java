64a2f78c78c051cee513ee87c425c1c7e1eeaa85
BZaganjori <bzaganjori@users.noreply.github.com>
2015-05-19 00:05:33 -0400

    Create changeReturnProgram.java

---
+++ numbers/changeReturnProgram.java
@@ -0,0 +1,26 @@
+package numbers;
+
+import java.util.Scanner;
+
+public class changeReturnProgram {
+	
+	private static String calculateChange(double cost, double paid) {
+		return String.format("%.2f", paid - cost);
+	}
+	
+	private static void prompt() {
+		Scanner scanner = new Scanner(System.in);
+		double cost, paid;
+		System.out.print("Cost of item: ");
+		cost = scanner.nextDouble();
+		System.out.print("Amount paid: ");
+		paid = scanner.nextDouble();
+		System.out.println("Customer change is: " + calculateChange(cost, paid));
+	}
+	
+	public static void main(String args[]) {
+		prompt();
+		System.exit(1);
+	}
+
+}
