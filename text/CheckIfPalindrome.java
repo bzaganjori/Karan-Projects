5cf4cde9d9d5da59de283b488ca1ad439e4b86e0
BZaganjori <bzaganjori@users.noreply.github.com>
2015-05-19 16:00:02 -0400

    Create CheckIfPalindrome.java

---
+++ text/CheckIfPalindrome.java
@@ -0,0 +1,19 @@
+package text;
+
+import java.util.Scanner;
+import text.ReverseString;
+
+public class CheckIfPalindrome {
+
+	public static boolean palindrome(String s) {
+		String input = ReverseString.reverse(s);
+		return input.equals(s);
+	}
+	
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		String in, s;
+		System.out.print("Please enter a string to check if palindrome: ");
+		System.out.println(palindrome(scanner.nextLine()));
+	}
+}
